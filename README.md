1a6196e325d72de12cb5f3e19b90e68f0ee40893
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-12-17 17:51:21 -0500

    basic file structure and files added

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # todolist
-To Do List HTML, CSS Vanilla Javascript
+To Do List HTML, CSS Vanilla Javascript webpack
+Created with OOP and SOLID design patterns
+to do item properties:
+- title
+- description
+- dueDate
+- priority
+- notes
+- checklist
+- seperate projects 
+- default project where all todos live
+
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: "./dist",
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    }
+};
